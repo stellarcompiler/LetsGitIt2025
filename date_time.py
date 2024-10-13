cb781c5d1adbb336d23bc0c7345bdde8f45477cb
stellarcompiler <aldrinantonyofficial7@gmail.com>
2024-10-13 12:55:36 +0530

    New file: Datetime

--- date_time.py
+++ date_time.py
@@ -1,7 +1,13 @@
 import datetime as dt
+import calendar as c
+
 date= dt.date.today()
 print(date.day ,"/" ,date.month, "/", date.year)
 print("October","", date.day,"",date.year)
 
-time=dt.time(10,30,55,4565)
+time=dt.time(10,30,55)
 print(time)
+
+yy=2024
+mm=10
+print(c.month(yy,mm))
